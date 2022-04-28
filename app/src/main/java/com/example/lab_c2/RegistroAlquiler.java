b30913eee077e6ee2258917a8ea216b24a410b9d
Empanada <ronald362001@gmail.com>
2022-04-27 20:37:16 -0600

    Fechas facheras facheritas

--- app/src/main/java/com/example/lab_c2/RegistroAlquiler.java
+++ app/src/main/java/com/example/lab_c2/RegistroAlquiler.java
@@ -1,11 +1,17 @@
 package com.example.lab_c2;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -17,17 +23,21 @@ import com.example.lab_c2.db.dbClientes;
 import com.example.lab_c2.db.dbVehiculos;
 import com.example.lab_c2.entidades.vehiculo;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class RegistroAlquiler extends AppCompatActivity {
     Button registro;
-    EditText fechaInicio, fechaFin, tiempoAlquiler;
-    TextView precioAlquiler;
+    TextView precioAlquiler,fechaInicio, fechaFin,tiempoAlquiler;
     Spinner idVehiculo,idCliente;
 
     ArrayList<String> listVehiculos,listClientes;
     dbVehiculos vehiculosDB = new dbVehiculos(this);
     dbClientes clientesDB = new dbClientes(this);
+    DatePickerDialog.OnDateSetListener setDateListener,setDateListener2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,13 @@ public class RegistroAlquiler extends AppCompatActivity {
         idVehiculo = findViewById(R.id.idVehiculo);
         idCliente = findViewById(R.id.idCliente);
 
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+
         listVehiculos = vehiculosDB.spinnerVehiculos();
         listClientes = clientesDB.spinnerClientes();
 
@@ -50,6 +67,105 @@ public class RegistroAlquiler extends AppCompatActivity {
 
         ArrayAdapter<String> adapterC = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, listClientes);
         idCliente.setAdapter(adapterC);
+
+        fechaInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegistroAlquiler.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setDateListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+
+                ;
+            }
+        });
+
+        fechaFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegistroAlquiler.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setDateListener2, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setDateListener = new DatePickerDialog.OnDateSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 = i1 + 1;
+                String date = "";
+
+                if(i2<10){
+                    date+="0"+i2;
+                }else{
+                    date+=i2;
+                }
+
+                if(i1<10){
+                    date+="/0"+i1;
+                }else{
+                    date+="/"+i1;
+                }
+
+                date += "/"+i;
+                fechaInicio.setText(date);
+
+                try {
+                    setTiempoAlquiler();
+                    getPrecioAlquiler();
+                }catch (Exception e){
+                    e.toString();
+                }
+            }
+        };
+
+        setDateListener2 = new DatePickerDialog.OnDateSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 = i1 + 1;
+                String date = "";
+
+                if(i2<10){
+                    date+="0"+i2;
+                }else{
+                    date+=i2;
+                }
+
+                if(i1<10){
+                    date+="/0"+i1;
+                }else{
+                    date+="/"+i1;
+                }
+
+                date += "/"+i;
+                fechaFin.setText(date);
+
+                try {
+                    setTiempoAlquiler();
+                    getPrecioAlquiler();
+                }catch (Exception e){
+                    e.toString();
+                }
+            }
+        };
+
+        /*
+        fechaInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegistroAlquiler.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        i1 = i1 + 1;
+                        String date = i + "/" + i1 + "/" + i2;
+                        fechaInicio.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });*/
 
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,48 +211,64 @@ public class RegistroAlquiler extends AppCompatActivity {
             }
         });
 
-        tiempoAlquiler.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int id = Integer.parseInt(idVehiculo.getSelectedItem().toString().split("-")[0]);
-                int tiempo = Integer.parseInt(tiempoAlquiler.getText().toString());
-
-                if (tiempo != 0) {
-                    dbVehiculos db = new dbVehiculos(RegistroAlquiler.this);
-                    vehiculo datos = db.findVehiculo("idV", String.valueOf(id));
-                    int precio = 0;
-
-                    if (datos != null) {
-                        String tipo = datos.getTipo();
-
-                        switch (tipo) {
-                            case "Coche":
-                                precio = 65 * tiempo;
-                                break;
-                            case "Microbus":
-                                precio = 65 * tiempo + 20;
-                                break;
-                            case "Furgoneta":
-                                precio = 70 * tiempo;
-                                break;
-                            case "Camión":
-                                precio = 75 * tiempo;
-                                break;
-                            default:
-                                precio = 0;
-                                break;
-                        }
-
-                    } else {
-                        Toast.makeText(RegistroAlquiler.this, "Error al calcular el precio", Toast.LENGTH_SHORT).show();
-                    }
-
-                    precioAlquiler.setText(String.valueOf(precio));
-                }
-            }
-        });
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setTiempoAlquiler(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate inicioD = LocalDate.parse(fechaInicio.getText().toString(),formatter);
+        LocalDate finalD = LocalDate.parse(fechaFin.getText().toString(),formatter);
 
+        Period periodo = null;
+
+        try {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                periodo = Period.between(inicioD, finalD);
+                Toast.makeText(this, String.valueOf(periodo.getDays()), Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.toString();
+        }
+
+        tiempoAlquiler.setText(String.valueOf(periodo.getDays()));
+    }
+
+    public void getPrecioAlquiler(){
+        int id = Integer.parseInt(idVehiculo.getSelectedItem().toString().split("-")[0]);
+        int tiempo = Integer.parseInt(tiempoAlquiler.getText().toString());
+
+        if (tiempo != 0) {
+            dbVehiculos db = new dbVehiculos(RegistroAlquiler.this);
+            vehiculo datos = db.findVehiculo("idV", String.valueOf(id));
+            int precio = 0;
+
+            if (datos != null) {
+                String tipo = datos.getTipo();
+
+                switch (tipo) {
+                    case "Coche":
+                        precio = 65 * tiempo;
+                        break;
+                    case "Microbus":
+                        precio = 65 * tiempo + 20;
+                        break;
+                    case "Furgoneta":
+                        precio = 70 * tiempo;
+                        break;
+                    case "Camión":
+                        precio = 75 * tiempo;
+                        break;
+                    default:
+                        precio = 0;
+                        break;
+                }
+
+            } else {
+                Toast.makeText(RegistroAlquiler.this, "Error al calcular el precio", Toast.LENGTH_SHORT).show();
+            }
+
+            precioAlquiler.setText(String.valueOf(precio));
+        }
+    }
 }
